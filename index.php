48c99d608c8696229ea40ebdd83cfd03506340aa
Patrick Dang <pdang2@mail.greenriver.edu>
2021-01-28 14:11:43 -0500

    added configuration file

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Name Patrick Dang
+ * Date: 1/28/2021
+ * Filename: index.php
+ * Description: Controller page for dating project
+ */
+
+//This is my CONTROLLER page
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the auto autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+$f3->set('Debug',3);
+
+//Define a default route (home page)
+$f3->route('GET /', function(){
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+//Run fat free
+$f3->run();
